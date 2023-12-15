0a317fe6793e3f40f07f40317f33fbc0c0720031
aliya <alexdavydov92@gmail.com>
2023-12-15 21:32:47 +0300

    продвижение? есть!

--- conftest.py
+++ conftest.py
@@ -1,10 +1,16 @@
 import allure
 from selene.support.shared import browser
 import pytest
+from selenium import webdriver
 
 
 @pytest.fixture()
-def open_browser():
+def browser_config():
+    browser.config.window_height = 720
+    browser.config.window_width = 1080
+
+@pytest.fixture()
+def open_browser(browser_config):
     with allure.step("Открываем главную страницу"):
         browser.open('https://github.com')
     yield
